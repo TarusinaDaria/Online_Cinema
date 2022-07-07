29aa8ce9f86fe2afb93e832ab3b7ee7a303d0719
TarusinaDaria <91415842+TarusinaDaria@users.noreply.github.com>
2022-07-07 11:22:17 +0300

    Commit

---
+++ Cinema/movies/urls.py
@@ -0,0 +1,18 @@
+"""
+from django.urls import path
+from .views import index
+
+urlpatterns = [
+path('', index),
+]
+"""
+
+
+from django.urls import path
+from .views import index_page, player_page
+
+
+urlpatterns = [
+    path('', index_page),
+    path('movie/<int:kinopoisk_id>', player_page)
+]
